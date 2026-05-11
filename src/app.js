2532de049ceb4189da8b9d67d002de4f470bf19b
BaraaMasri99 <s12400192@stu.najah.edu>
2026-05-11 17:19:44 +0300

    add facebook login flow

--- src/app.js
+++ src/app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
 const { setupSwagger } = require("./docs/swagger");
+const { passport, configurePassport } = require("./config/passport");
 const { sendSuccess, sendError } = require("./utils/apiResponse");
 
 const app = express();
+configurePassport();
 
 app.use(cors());
 app.use(express.json());
+app.use(passport.initialize());
 setupSwagger(app);
 
 app.get("/health", (req, res) => {
